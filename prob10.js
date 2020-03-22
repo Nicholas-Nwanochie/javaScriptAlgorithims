abc19f2472144e222f95134c4a4eba610e34c721
NICHOLAS NWANOCHIE <nickclay@live.com>
2020-03-21 22:55:19 -0500

    Create prob10.js
    Write a JavaScript program to check from two given integers whether one of them is 8 or their sum or difference is 8

---
+++ prob10.js
@@ -0,0 +1,17 @@
+function check8(x, y) {
+  if (x == 8 || y == 8) {
+    return true;
+  }
+
+  if (x + y == 8 || Math.abs(x - y) == 8)
+  {
+    return true;
+  }
+
+  return false;
+}
+
+console.log(check8(7, 8));
+console.log(check8(16, 8));
+console.log(check8(24, 32));
+console.log(check8(17, 18));
